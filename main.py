3aaf15e88ded476d002a73d1c4b686c3eedefc91
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-04-01 11:59:02 +0200

    Rectification

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# recuperation des information de l'utilisateur
+# Récupération des informations de l'utilisateur
 nom = input("Quel est votre nom? ")
 prenom = input("Quel est votre prénom? ")
 age = input("Quel est votre age? ")
-# combinaison des nom dans une variable
+# Combinaison des noms dans une variable
 nom_complet = prenom + " " + nom
-# affichage du nom et prenom de l'uitilisateur
+# Affichage du nom et prénom de l'utilisateur
 print("Bonjour ",nom_complet ," je suis votre assistant virtuel!") 
 print("Votre age est"  ,age, "ans")

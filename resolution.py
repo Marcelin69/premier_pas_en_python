ebd25c3ef0f0305efc1dcecc321351952563da22
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-04-01 16:13:58 +0200

    Résolution des problèmes en python

--- resolution.py
+++ resolution.py
@@ -1,10 +1,13 @@
 #Erreur 1
-print("Test d'affichage)
+print("Test d'affichage")
 
 #Erreur 2
-1page = 100
+page = 100
 
 #Erreur 3
 achat_1 = 12.4
 achat_2 = 7.5
+achat_3 = 5.6
 prix = achat_1 + achat_2 + achat_3
+
+print(prix)

9751dbab63d2ba934343e69b66d2fdcfd7a9344f
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-04-01 12:37:57 +0200

    calculez l'aire d'un cercle

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 nom = input("Quel est votre nom? ")
 prenom = input("Quel est votre prénom? ")
 age = input("Quel est votre age? ")
+rayon = float(input("Quel est le rayon de votr cercle? "))
 # Combinaison des noms dans une variable
 nom_complet = prenom + " " + nom
 # Affichage du nom et prénom de l'utilisateur
 print("Bonjour ",nom_complet ," je suis votre assistant virtuel!") 
 print("Votre age est"  ,age, "ans")
+
+# calculé l'aire d'un cercle 
+pi = 3.14
+aire = (pi*rayon)**2
+print ("L'aire de votre cercle est de", aire, "cm²")

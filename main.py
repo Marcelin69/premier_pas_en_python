fc69c0657f5b7473a50520ea33894b82106ee6c2
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-04-01 11:56:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# recuperation des information de l'utilisateur
+nom = input("Quel est votre nom? ")
+prenom = input("Quel est votre prénom? ")
+age = input("Quel est votre age? ")
+# combinaison des nom dans une variable
+nom_complet = prenom + " " + nom
+# affichage du nom et prenom de l'uitilisateur
+print("Bonjour ",nom_complet ," je suis votre assistant virtuel!") 
+print("Votre age est"  ,age, "ans")

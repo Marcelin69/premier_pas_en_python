cf7dc8ef17f551174b85eaa42eda1a18351374eb
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-04-03 09:58:20 +0200

    Réalisation d'une calculatrice v1

---
+++ calculatrice_v1.py
@@ -0,0 +1,53 @@
+nbre_1 = int(input("Entrez un nombre  : "))
+nbre_2 =  int(input("Entrez un nombre : "))
+
+# operateur = input("Entrez votre operatuer mathematique : ")
+
+# (operateur == "+")
+
+# Addition
+sommeadd = nbre_1 + nbre_2
+print("La somme ",nbre_1," + ", nbre_2 ," est :",sommeadd )
+# soustraction
+sommesous = nbre_1 - nbre_2
+print("La soustraction ",nbre_1," - ", nbre_2 ," est :",sommesous )
+# multiplication
+sommemult = nbre_1 * nbre_2
+print("La multiplication ",nbre_1," * ", nbre_2 ," est :",sommemult )
+# Division
+sommediv = nbre_1 / nbre_2
+print("La division ",nbre_1," / ", nbre_2 ," est :",sommediv )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# print(random.randint(0,9))
